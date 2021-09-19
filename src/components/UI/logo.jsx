9832ce95790c88459cfc93064402c6dfd1a9e21b
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2021-09-18 22:08:16 -0600

    Update logo.jsx

--- src/components/UI/logo.jsx
+++ src/components/UI/logo.jsx
@@ -5,7 +5,15 @@ const Logo = () => (
   <>
     <NextLink href="/" passHref>
       <chakra.a fontSize="2rem" fontWeight="700">
-        LOGO
+        <NextImage
+          src="/logo.png"
+          width="150"
+          height="50"
+          alt="logo"
+          placeholder="blur"
+          blurDataURL="L5I~of#i004mgjw]-4XA00?wL#xu"
+          priority
+        />
       </chakra.a>
     </NextLink>
   </>

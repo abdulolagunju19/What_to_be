be7c63d24de6dad6bcbdf1c84170a06473010734
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2021-09-18 23:30:02 -0600

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -105,7 +105,6 @@ const Home = () => {
               mx={{ base: "auto", lg: "0" }}
               pl={{ lg: isOdd(index) == 1 && "10" }}
               pr={{ lg: isOdd(index) == 0 && "10" }}
-              borderRadius="lg"
             >
               <NextImage
                 src={item.image}

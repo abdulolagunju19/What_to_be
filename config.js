34156e3793e1d26b493fd91ecd31f9494bb08202
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2021-09-18 23:35:12 -0600

    Update config.js

--- config.js
+++ config.js
@@ -10,19 +10,19 @@ export const data = [
     title: "Learn about your Professors.",
     description:
       "Tired of scraping the University of Alberta faculty pages in order to learn about those who teach you? We make it easy, and more informative.",
-    image: "/code-review-bro.png",
+    image: "/Breast cancer research-bro.png",
   },
   {
     title: "Learn about your clubs on campus.",
     description:
       "Bearsden gives you a tiny bit of information about the clubs you want to join. We give you their purpose, and follow up with them so you can learn about the interesting things people are trying to put together.",
-    image: "/club.jpg",
+    image: "/Creative team-cuate.png",
   },
   {
     title: "Get a glimpse into what we do here.",
     description:
       "The University of Alberta is filled with amazing people doing amazing things every day. It's time we shared these things. Support us by contacting us if you want to be featured on our website.",
-    image: "/contact-us.png",
+    image: "/Get in touch-amico.png",
   },
 ]
 

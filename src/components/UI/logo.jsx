9ce9c62ac935bf74fc4de7a17c9d10c02afe10ef
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2021-09-20 11:55:47 -0600

    Update logo.jsx

--- src/components/UI/logo.jsx
+++ src/components/UI/logo.jsx
@@ -5,7 +5,7 @@ import NextImage from "next/image"
 const Logo = () => (
   <>
     <NextLink href="/" passHref>
-      <chakra.a fontSize="2rem" fontWeight="700">
+      <chakra.a fontSize="1rem" fontWeight="700">
         What it's Like to Be...
       </chakra.a>
     </NextLink>

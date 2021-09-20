b94d44279d5db3cbd98136a93c487940a2614e17
abdulolagunju19 <86393716+abdulolagunju19@users.noreply.github.com>
2021-09-20 12:47:16 -0600

    Update config.js

--- config.js
+++ config.js
@@ -9,19 +9,19 @@ export const data = [
   {
     title: "Learn about your Professors.",
     description:
-      "Tired of scraping the University of Alberta faculty pages in order to learn about those who teach you? We make it easy, and more informative.",
+      "Tired of scraping the University of Alberta faculty pages in order to learn about interesting research going on in campus? We hope to provide you with a wide variety of cool research and the personalities behind them.",
     image: "/Breast cancer research-bro.png",
   },
   {
     title: "Learn about your clubs on campus.",
     description:
-      "Bearsden gives you a tiny bit of information about the clubs you want to join. We give you their purpose, and follow up with them so you can learn about the interesting things people are trying to put together.",
+      "Bearsden gives you a little bit of information about the clubs you want to join. Let's take it up a notch. We give you their purpose, and follow up with them so you can learn more about the interesting things people are trying to put together.",
     image: "/Creative team-cuate.png",
   },
   {
     title: "Get a glimpse into what we do here.",
     description:
-      "The University of Alberta is filled with amazing people doing amazing things every day. It's time we shared these things. Support us by contacting us if you want to be featured on our website.",
+      "The University of Alberta is filled with amazing people doing amazing things every day. It's time we shared these things. Support us by contacting us if you want to be featured on our website, or have any ideas about taking this project to the next level.",
     image: "/Get in touch-amico.png",
   },
 ]
